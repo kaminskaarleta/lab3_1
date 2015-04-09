a7b8774b7933a409abeef4c9f98c332c9331b9af
Kamińska <180195@edu.p.lodz.pl>
2015-04-09 21:24:54 +0200

    Added new test class to BookKeeper class

---
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -0,0 +1,27 @@
+package lab3_1;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class BookKeeperTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testState_invoiceWithOneItem_shouldBeOneItem() {
+		
+
+	}
+
+}
