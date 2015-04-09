a24f4090f8a243a5d7057d73113d6a959a6d4a1b
Kamińska <180195@edu.p.lodz.pl>
2015-04-09 21:29:40 +0200

    Changed access modifier in ProductDate constructor on public.

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -3,9 +3,19 @@ package lab3_1;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 
 public class BookKeeperTest {
@@ -20,7 +30,12 @@ public class BookKeeperTest {
 
 	@Test
 	public void testState_invoiceWithOneItem_shouldBeOneItem() {
-		
+		ClientData clientData = new ClientData(Id.generate(), "Arleta");
+		Invoice invoice = new Invoice(Id.generate(), clientData);
+		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+		Money price = new Money(5);
+		ProductData productData = new ProductData(Id.generate(), price, "kanapka", ProductType.FOOD, new Date());
+
 
 	}
 
