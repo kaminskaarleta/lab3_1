ff4268ac6e843cd7b68eb87bb4b7cbd21927537c
Kamińska <180195@edu.p.lodz.pl>
2015-04-09 21:55:29 +0200

    Finished test state to invoice with one item

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -30,12 +31,24 @@ public class BookKeeperTest {
 
 	@Test
 	public void testState_invoiceWithOneItem_shouldBeOneItem() {
+		
 		ClientData clientData = new ClientData(Id.generate(), "Arleta");
 		Invoice invoice = new Invoice(Id.generate(), clientData);
 		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
 		ProductData productData = new ProductData(Id.generate(), new Money(2), "kanapka", ProductType.FOOD, new Date());
-		RequestItem item = new RequestItem(productData, 0, new Money(5));
-
+		RequestItem item = new RequestItem(productData, 0, new Money(2));
+		invoiceRequest.add(item);
+		
+		InvoiceFactory invoiceFactory = Mockito.mock(InvoiceFactory.class);
+		TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
+		
+		Mockito.when(invoiceFactory.create(invoiceRequest.getClientData())).thenReturn(invoice);
+		Mockito.when(taxPolicy.calculateTax(ProductType.FOOD, new Money(2)))
+				.thenReturn(new Tax(new Money(2), "kanapka"));
+		BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+		Invoice newInvoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+		
+		assertThat(newInvoice.getItems().size(), is(1));
 	}
 
 }
