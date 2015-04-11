a2fa7db812a4cb36a5a5ef05622569378c68d488
Kamińska <180195@edu.p.lodz.pl>
2015-04-11 13:57:08 +0200

    Added new state test to AddProductCommandHandlerTest for validate addition to reservation

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -1,5 +1,6 @@
 package pl.com.bottega.ecommerce.sales.application.api.handler;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 import java.util.Date;
@@ -7,34 +8,73 @@ import java.util.Date;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation.ReservationStatus;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AddProductCommandHandlerTest {
-
+	
+	Id id;
+	AddProductCommand command;
+	ClientData clientData;
+	Reservation reservation;
+	Product product;
+	
+	@InjectMocks
+	AddProductCommandHandler addProductCommandHandler = new AddProductCommandHandler();
+	
+	@Mock
+	ReservationRepository reservationRepository;
+	@Mock
+	ProductRepository productRepository;
+	@Mock
+	ClientRepository clientRepository;
+	@Mock
+	SuggestionService suggestionService;
+	@Mock
+	SystemContext systemContext;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		
+		id = Id.generate();
+		command = new AddProductCommand(id, id, 5);
+		clientData = new ClientData(id, "Arleta");
+		reservation = new Reservation(id, ReservationStatus.OPENED, clientData, new Date());
+		product = new Product(id, new Money(5), "sandwich", ProductType.FOOD);
 	}
 
 	@Test
-	public void stateTest() {
-		AddProductCommand addProductCommand = new AddProductCommand(Id.generate(), Id.generate(), 5);
-		Reservation reservation = new Reservation(Id.generate(), ReservationStatus.OPENED, 
-				new ClientData(Id.generate(), "Arleta"), new Date());
-		
-		Product product = new Product(Id.generate(), new Money(5), "sandwich", ProductType.FOOD);
+	public void stateTest_addOneProduct_shouldContainsThatProduct() {	
+		MockitoAnnotations.initMocks(addProductCommandHandler);
+		Mockito.when(reservationRepository.load(command.getOrderId())).thenReturn(reservation);
+		Mockito.when(productRepository.load(command.getProductId())).thenReturn(product);
 
+		addProductCommandHandler.handle(command);
+		assertThat(reservation.contains(product), is(true));
 	}
 
 }
