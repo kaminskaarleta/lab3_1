17ebd33eef00dd038d577f1d93f670c96dfada81
Kamińska <180195@edu.p.lodz.pl>
2015-04-11 09:47:21 +0200

    Added new test class for AddProductCommandHandler class.

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,24 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AddProductCommandHandlerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
